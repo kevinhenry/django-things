f3088b3b73d49ebfeb5566042e9c2c01459562c1
Kevin Henry <kevinhenry@mac.com>
2021-08-23 09:08:23 -0700

    Initial Commit

---
+++ things/tests.py
@@ -0,0 +1,21 @@
+from django.http import response
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+# Create your tests here.
+class ThingsTest(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_about_page_status_code(self):
+        url = reverse("about")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
